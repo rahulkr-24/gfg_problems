5a8a7a8fae24a565488f4ae0dbf4dd656dc0f82e
Rahul Kumar <128255986+rahulkr-24@users.noreply.github.com>
2024-03-04 20:56:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fraction pairs with sum 1/fraction-pairs-with-sum-1.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    int countFractions(int n, int num[], int den[])
+    {
+       unordered_map<double,int> mp;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            double nm = num[i];
+            double dm = den[i];
+            double x = (dm-nm)/dm;
+            double y = nm/dm;
+            if(mp[x]>0){
+                cnt+=mp[x];
+            }
+            mp[y]++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int numerator[n],denominator[n];
+        for(int i=0;i<n;i++)
+            cin>>numerator[i];
+        for(int i=0;i<n;i++)
+            cin>>denominator[i];
+        Solution ob;
+        int ans=ob.countFractions(n,numerator,denominator);
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
