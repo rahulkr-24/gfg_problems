b8f14ebf47ae0e611a9a5d3c606d43e06bf33e30
Rahul Kumar <128255986+rahulkr-24@users.noreply.github.com>
2024-06-13 01:34:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    int mod=1e9+7;
+    int padovanSequence(int n)
+    {
+       //code here
+       vector<int>dp(n+1,-1);
+       dp[0]=dp[1]=dp[2]=1;
+       if(dp[n]!=-1)return dp[n]%mod;
+       for(int i=3;i<=n;i++)
+       {
+           dp[i]=(dp[i-2]+dp[i-3])%mod;
+       }
+       return dp[n]%mod;
+       
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout<<ob.padovanSequence(n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
